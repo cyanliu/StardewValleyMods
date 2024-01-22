4cfff5cead30bd876364c7af839e8ccba8f26b5f
Christina Liu <christinaa.liuu@gmail.com>
2024-01-22 13:19:14 -0500

    now with the ability to find where the fridge is

--- Fridge/ModEntry.cs
+++ Fridge/ModEntry.cs
@@ -4,6 +4,7 @@ using StardewModdingAPI;
 using StardewModdingAPI.Events;
 using StardewModdingAPI.Utilities;
 using StardewValley;
+using StardewValley.Locations;
 
 namespace Fridge
 {
@@ -21,20 +22,60 @@ namespace Fridge
         }
 
 
+        private ICursorPosition cursorPos;
+        private Point fridgePos;
         /*********
         ** Private methods
         *********/
         /// <summary>Raised after the player presses a button on the keyboard, controller, or mouse.</summary>
         /// <param name="sender">The event sender.</param>
         /// <param name="e">The event data.</param>
-        private void OnButtonPressed(object sender, ButtonPressedEventArgs e)
+        private void OnButtonPressed(object sender, ButtonPressedEventArgs e )
         {
             // ignore if player hasn't loaded a save yet
             if (!Context.IsWorldReady)
                 return;
 
             // print button presses to the console window
-            this.Monitor.Log($"{Game1.player.Name} pressed {e.Button}.", LogLevel.Debug);
+
+            if (Game1.player.ActiveObject != null )
+            {
+                if ((Game1.player.ActiveObject.DisplayName == "Wood Sign" || Game1.player.ActiveObject.DisplayName == "Stone Sign") && e.Button.IsActionButton())
+                {
+                    // https://stardewvalleywiki.com/Modding:Modder_Guide/APIs/Input#ICursorPosition
+                    cursorPos = this.Helper.Input.GetCursorPosition();
+
+
+                    this.Monitor.Log($"MR pressed at {cursorPos.GrabTile.X} while holding sign");
+                    this.Monitor.Log($"{this.Helper.Input.GetCursorPosition()}");
+                    
+                    this.Monitor.Log($"{Game1.currentLocation.NameOrUniqueName}");
+
+                    // finding location of fridge, and check if it matches the grab position
+                    if (Game1.currentLocation is FarmHouse house)
+                    {
+                        fridgePos = house.fridgePosition;
+                        
+                        this.Monitor.Log($"{fridgePos}", LogLevel.Debug);
+                        if (fridgePos.X == cursorPos.GrabTile.X && fridgePos.Y == cursorPos.GrabTile.Y)
+                        {
+                            this.Helper.Input.Suppress(SButton.MouseRight);
+                            this.Monitor.Log("Action Button suppressed at fridge");
+                        }
+
+                        // TODO: check for mini fridge
+                        // BigCraftable | Mini-Fridge | 216
+
+                    }
+
+                }
+                else
+                {
+                    this.Monitor.Log($"{Game1.player.Name} pressed {e.Button} while holding {Game1.player.ActiveObject.DisplayName}.", LogLevel.Debug);
+
+                }
+            }
+
         }
     }
 }
